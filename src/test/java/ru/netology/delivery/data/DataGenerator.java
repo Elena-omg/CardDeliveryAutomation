2bc9b3d96d58f53ae3a6084be5338463a873ef3b
Elena <elena_golubkova1982@mail.ru>
2026-04-01 21:24:20 +0400

    Сhanges added

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -12,12 +12,10 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
-    // Метод для генерации даты: берем текущую и прибавляем shift дней
     public static String generateDate(int shift) {
         return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    // Метод для выбора города из списка (так надежнее, чем просто Faker)
     public static String generateCity() {
         var cities = new String[]{
                 "Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург",
@@ -27,14 +25,11 @@ public class DataGenerator {
         return cities[new Random().nextInt(cities.length)];
     }
 
-    // Метод для генерации Имени и Фамилии через Faker
     public static String generateName(Faker faker) {
-        // Используем lastName + firstName и заменяем 'ё' на 'е' для стабильности формы
         String name = faker.name().lastName() + " " + faker.name().firstName();
         return name.replace("ё", "е");
     }
 
-    // Метод для генерации телефона
     public static String generatePhone(Faker faker) {
         return faker.phoneNumber().phoneNumber();
     }
@@ -47,12 +42,10 @@ public class DataGenerator {
 
         public static UserInfo generateUser(String locale) {
             faker = new Faker(new Locale(locale));
-            // УБИРАЕМ faker из скобок у generateCity()
             return new UserInfo(generateCity(), generateName(faker), generatePhone(faker));
         }
     }
 
-    // Тот самый класс данных с аннотацией Lombok
     @Value
     public static class UserInfo {
         String city;
--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -11,23 +11,20 @@ class CardDeliveryTest {
 
     @BeforeEach
     void setup() {
-        // Открываем страницу перед каждым тестом
         open("http://localhost:9999");
     }
 
     @Test
     @DisplayName("Should replan meeting")
     void shouldReplanMeeting() {
-        // 1. Подготовка данных через наш генератор
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
 
-        // 2. Первичное планирование встречи
+
         $("[data-test-id=city] input").setValue(validUser.getCity());
-        // Очистка поля даты (стандартный способ для таких инпутов)
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(firstMeetingDate);
         $("[data-test-id=name] input").setValue(validUser.getName());
@@ -35,26 +32,23 @@ class CardDeliveryTest {
         $("[data-test-id=agreement]").click();
         $$("button").find(Condition.exactText("Запланировать")).click();
 
-        // Проверка сообщения об успехе
         $("[data-test-id=success-notification]")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate));
 
-        // 3. Перепланирование на другую дату
+
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $$("button").find(Condition.exactText("Запланировать")).click();
 
-        // Проверка появления окна перепланирования
         $("[data-test-id=replan-notification]")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
 
-        // Нажимаем кнопку "Перепланировать" во всплывающем окне
-        // Вместо обычного .click() по кнопке
-        $("[data-test-id='replan-notification'] .button").shouldBe(Condition.visible).click();
 
-        // 4. Финальная проверка сообщения об успехе с новой датой
+
+        $("[data-test-id='replan-notification'] .button").shouldBe(Condition.visible).click();
+        
         $("[data-test-id=success-notification]")
                 .shouldBe(Condition.visible)
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate));
